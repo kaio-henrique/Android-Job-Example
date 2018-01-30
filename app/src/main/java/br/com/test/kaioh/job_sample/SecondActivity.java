1a1b6cf139e50260c09a2daaa0620a2720751d9a
Kaio Henrique <kaiohenriquedesousacosta3@gmail.com>
2018-01-30 14:56:04 -0300

    - Segunda Activity criada

---
+++ app/src/main/java/br/com/test/kaioh/job_sample/SecondActivity.java
@@ -0,0 +1,18 @@
+package br.com.test.kaioh.job_sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by @autor Kaio Henrique on 30/01/2018.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
